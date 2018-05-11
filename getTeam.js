f508e4ffb4690a9fcc1b4e075e97f7565c1eba00
Sindre Øye Svendby <sinsvend@gmail.com>
2018-05-11 20:14:02 +0200

    This is working

--- getTeam.js
+++ getTeam.js
@@ -1,4 +1,3 @@
-
 const log = require("debug")("getTeam");
 require("dotenv").config();
 const { API_URL } = process.env;
@@ -6,16 +5,15 @@ const { API_URL } = process.env;
 const axios = require("axios");
 
 async function apiGetTournament(id) {
-    const tournaments = await apiGetTournaments(`?TurneringsId=${id}&Lag=True`);
-    const tournamentArray = tournaments.filter(
-      ({ TurneringsId }) => TurneringsId == id
-    );
-    if (tournamentArray.length === 0) {
-      return { noSuchTournament: "No such tournamentID" };
-    }
-    return tournamentArray[0];
+  const tournaments = await apiGetTournaments(`?TurneringsId=${id}&Lag=True`);
+  const tournamentArray = tournaments.filter(
+    ({ TurneringsId }) => TurneringsId == id
+  );
+  if (tournamentArray.length === 0) {
+    return { noSuchTournament: "No such tournamentID" };
+  }
+  return tournamentArray[0];
 }
-
 
 async function apiGetTournaments(extraQueryString = "") {
   log(`request URL: ${API_URL}/tournaments${extraQueryString}`);
@@ -24,79 +22,117 @@ async function apiGetTournaments(extraQueryString = "") {
   return data;
 }
 
-
 function getData(result) {
-    if (result.status !== 200) {
-      log(`Did not get a 200 response from API, details: ${result}`);
-      throw new Error(`Did not get a 200 response from API, details: ${result}`);
-    }
-    return result.data;
+  if (result.status !== 200) {
+    log(`Did not get a 200 response from API, details: ${result}`);
+    throw new Error(`Did not get a 200 response from API, details: ${result}`);
+  }
+  return result.data;
 }
 
+const testID = 193;
+const osloRT = 198;
 
-const testID = 193
-const osloRT = 198
+async function apiGetPlayer(id) {
+  const players = await apiGetPlayers();
+  const playersArray = players.filter(({ SpillerId }) => SpillerId == id);
+
+  if (playersArray.length === 0) {
+    return { noSuchPlayer: "No such player id" };
+  }
+  return playersArray[0];
+}
+
+let players = false;
+async function apiGetPlayers() {
+  if (!players) {
+    const result = await axios.get(`${API_URL}/players`);
+    players = getData(result);
+  }
+  return players;
+}
 
 async function main() {
-    const result = await apiGetTournament(osloRT)
-    
-    const kvinneKlassen = result["Klasser"][0]["Lag"]
-    const gutteKlassen = result["Klasser"][1]["Lag"]
-    log(`K: ${kvinneKlassen}`)
-    log(`G: ${gutteKlassen}`)
-    for (let team of gutteKlassen) {
-        const {Klasse, Spiller_1, Spiller_2, Lagnavn} = team
-        const spiller1Navn = Lagnavn.split("/")[0].trim()
-        const spiller2Navn = Lagnavn.split("/")[1].trim()
-        const firstName1 = spiller1Navn.split(" ").reverse().slice(1).reverse().join(" ").trim()
-        const firstName2 = spiller2Navn.split(" ").reverse().slice(1).reverse().join(" ").trim()
-        const lastName1 = spiller1Navn.split(" ").reverse()[0].trim()
-        const lastName2 = spiller2Navn.split(" ").reverse()[0].trim()
-        // log("------")
-        // log(Klasse)
-        // log(Spiller_1)
-        // log(spiller1Navn)
-        // log(lastName1)
-        // log(Spiller_2)
-        // log(spiller2Navn)
-        // log(lastName2)
-        await runNightwatch({
-            Klasse,
-            Spiller_1,
-            Spiller_2,
-            firstName1,
-            firstName2,
-            lastName1,
-            lastName2
-        })
-    }
+  const result = await apiGetTournament(osloRT);
+  const kvinneKlassen = result["Klasser"][0]["Lag"];
+  const gutteKlassen = result["Klasser"][1]["Lag"];
+  log(`K: ${kvinneKlassen}`);
+  log(`G: ${gutteKlassen}`);
+  const both = gutteKlassen.concat(kvinneKlassen);
+  for (let team of both) {
+    const { Klasse, Spiller_1, Spiller_2, Lagnavn } = team;
+    const p1 = await apiGetPlayer(Spiller_1);
+    const { ProfixioId: profixioIdSpiller1 } = p1;
 
+    const p2 = await apiGetPlayer(Spiller_2);
+    const { ProfixioId: profixioIdSpiller2 } = p2;
+    const spiller1Navn = Lagnavn.split("/")[0].trim();
+    const spiller2Navn = Lagnavn.split("/")[1].trim();
+    const firstName1 = spiller1Navn
+      .split(" ")
+      .reverse()
+      .slice(1)
+      .reverse()
+      .join(" ")
+      .trim();
+    const firstName2 = spiller2Navn
+      .split(" ")
+      .reverse()
+      .slice(1)
+      .reverse()
+      .join(" ")
+      .trim();
+    const lastName1 = spiller1Navn
+      .split(" ")
+      .reverse()[0]
+      .trim();
+    const lastName2 = spiller2Navn
+      .split(" ")
+      .reverse()[0]
+      .trim();
+    // log("------")
+    // log(Klasse)
+    // log(Spiller_1)
+    // log(spiller1Navn)
+    // log(lastName1)
+    // log(Spiller_2)
+    // log(spiller2Navn)
+    // log(lastName2)
+    await runNightwatch({
+      Klasse,
+      profixioIdSpiller1,
+      profixioIdSpiller2,
+      firstName1,
+      firstName2,
+      lastName1,
+      lastName2
+    });
+  }
 }
 
-main()
+main();
 
-
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 async function runNightwatch(args) {
-    const  {    
-        Klasse,
-        Spiller_1,
-        Spiller_2,
-        firstName1,
-        firstName2,
-        lastName1,
-        lastName2
-    } = args
+  const {
+    Klasse,
+    profixioIdSpiller1,
+    profixioIdSpiller2,
+    firstName1,
+    firstName2,
+    lastName1,
+    lastName2
+  } = args;
 
-  const command = `-s "${Klasse}" -s "${Spiller_1}" -s "${firstName1}" -s "${lastName1}" -s "${Spiller_2}" -s "${firstName2}" -s "${lastName2}"`
+  const command = `-s "${Klasse}" -s "${profixioIdSpiller1}" -s "${firstName1}" -s "${lastName1}" -s "${profixioIdSpiller2}" -s "${firstName2}" -s "${lastName2}"`;
   try {
-        console.log(`nightwatch main.js ${command}`)
-        //const { stdout, stderr } = await exec(`nightwatch main.js ${command}`);
-        //console.log('stdout:', stdout);
-        //console.log('stderr:', stderr);
-  } catch(err) {
-      console.log(`Catch error on ${spiller1Navn} ${spiller2Navn} continue with next` )
+    console.log(`nightwatch main.js ${command}`);
+    //const { stdout, stderr } = await exec(`nightwatch main.js ${command}`);
+    //console.log('stdout:', stdout);
+    //console.log('stderr:', stderr);
+  } catch (err) {
+    console.log(`Catch error on ${lastName1} ${lastName2} continue with next`);
   }
- }
+}
--- main.js
+++ main.js
@@ -5,18 +5,20 @@ if (!URL || !USERNAME || !PASSWORD) {
   console.log("URL, USERNAME, PASSWORD needs to be set as env.var");
 }
 
-const klasse = process.argv[4]
+const klasse = process.argv[4];
 
-const player1id = process.argv[6]
+const player1id = process.argv[6];
 
-const firstName1 = process.argv[8]
-const lastName1 = process.argv[10]
+const firstName1 = process.argv[8];
+const lastName1 = process.argv[10];
 
-const player2id = process.argv[12]
-const firstName2 = process.argv[14]
-const lastName2 = process.argv[16]
+const player2id = process.argv[12];
+const firstName2 = process.argv[14];
+const lastName2 = process.argv[16];
 
-console.log(`Args: ${klasse} ${player1id} ${firstName1} ${lastName1} ${player2id} ${firstName2} ${lastName2}`)
+console.log(
+  `Args: ${klasse} ${player1id} ${firstName1} ${lastName1} ${player2id} ${firstName2} ${lastName2}`
+);
 
 module.exports = {
   // get a session, that tell cupassist which tournament we want to interact with
@@ -25,10 +27,12 @@ module.exports = {
   },
 
   login: function(browser) {
+    let player1Id = false;
+    let player2Id = false;
     let browserCookies = false;
     browser
       .url("https://www.profixio.com/pamelding/reg_login_form.php")
-      .waitForElementVisible("body", 1000)
+      .waitForElementVisible("body", 3000)
       .assert.visible("input#brukernavn")
       .assert.visible("input#pwdinput")
       .assert.visible("input#pwdinput")
@@ -38,58 +42,70 @@ module.exports = {
       .click("input[type=submit]")
       .assert.visible("input")
       .click("input")
-      .pause(2000)
-    .url("https://www.profixio.com/pamelding/svb_velg_spillere.php")
-    .waitForElementVisible("body", 2000);
+      .pause(1200)
+      .url("https://www.profixio.com/pamelding/svb_velg_spillere.php")
+      .waitForElementVisible("body", 3000)
+      .url("https://www.profixio.com/pamelding/svb_sok_spillere.php")
+      .waitForElementVisible("body", 3000)
+      .setValue("input[type=text]", "Svendby")
+      .click("input[type=submit]")
+      .execute(
+        function(args) {
+          return document.getElementsByTagName("option")[0].value;
+        },
+        [],
+        result => {
+          console.log(`Result is ${JSON.stringify(result.value)}`);
+          player1Id = result.value;
+        }
+      )
+      .url("https://www.profixio.com/pamelding/svb_sok_spillere.php")
+      .waitForElementVisible("body", 3000)
+      .setValue("input[type=text]", "Tveitan")
+      .click("input[type=submit]")
+      .execute(
+        function(args) {
+          return document.getElementsByTagName("option")[0].value;
+        },
+        [],
+        result => {
+          console.log(`Result is ${JSON.stringify(result.value)}`);
+          player2Id = result.value;
+        }
+      )
+      .getCookies(function callback(result) {
+        browserCookies = result.value;
+      })
+      .url("https://www.profixio.com/pamelding/svb_velg_spillere.php")
+      .waitForElementVisible("body", 3000);
     browser
       .perform(function(done) {
         browser.execute(
-          function(player1id, player2id, lastName1, lastName2, firstName1, firstName2) {
-            document.getElementsByTagName("input")[0].value = player1id;
-            document.getElementsByTagName("input")[1].value = player2id;
+          function(id1, id2, lastName1, firstName1, lastName2, firstName2) {
+            document.getElementsByTagName("input")[0].value = id1;
+            document.getElementsByTagName("input")[1].value = id2;
             document.getElementsByTagName("input")[2].value = lastName1;
             document.getElementsByTagName("input")[3].value = firstName1;
             document.getElementsByTagName("input")[4].value = lastName2;
             document.getElementsByTagName("input")[5].value = firstName2;
             document.getElementById("subm_btt").disabled = false;
-            return true;
+            return args;
           },
-          [player1id, player2id, lastName1, lastName2, firstName1, firstName2],
+          [player1id, player2id, lastName1, firstName1, lastName2, firstName2],
           done
         );
       })
-      .click("input#subm_btt")     
-      .pause(45000)
-      // .click(`select option[value=${klasse}]`)
-      // .setValue("input[name=lag_kontakt_navn", ".")
-      // .setValue("input[name=lag_kont_epost", "post@osvb.no")
-      // .setValue("input[name=lag_kont_mob", "93008598")
-      // .click("input[type=submit]");
+
+      .click("input#subm_btt")
+      .pause(1200)
+      .url("https://www.profixio.com/pamelding/svb_reg_lag_form.php?a=new2")
+      .waitForElementVisible("input", 3000)
+      .click(`select option[value=${klasse}]`)
+      .setValue("input[name=lag_kontakt_navn", ".")
+      .setValue("input[name=lag_kont_epost", "spam@spam.com")
+      .setValue("input[name=lag_kont_mob", "93008598")
+      .click("input[type=submit]");
 
     browser.end();
   }
 };
-
-
-// document.getElementsByTagName("input")[0].value = "2530";
-// document.getElementsByTagName("input")[1].value = "2822";
-// document.getElementsByTagName("input")[2].value = "Iversen";
-// document.getElementsByTagName("input")[3].value = "Ragnar";
-// document.getElementsByTagName("input")[4].value = "Tveitan";
-// document.getElementsByTagName("input")[5].value = "Håkon";
-// document.getElementById("subm_btt").disabled = false;
-
-// var domNode = document.getElementById("subm_btt")
-
-// var newEmailInput1 = document.createElement('input');
-// newEmailInput1.setAttribute("type", "text");
-// newEmailInput1.setAttribute("name", "epost1");
-// newEmailInput1.setAttribute("value", "post@osvb.no");
-
-// var newEmailInput2 = document.createElement('input');
-// newEmailInput2.setAttribute("type", "text");
-// newEmailInput2.setAttribute("name", "epost2");
-// newEmailInput2.setAttribute("value", "post@osvb.no");
-
-// domNode.parentNode.insertBefore(newEmailInput1, domNode.nextSibling);  
-// domNode.parentNode.insertBefore(newEmailInput2, domNode.nextSibling);  
